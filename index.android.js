11acab7a1e9355bef789a872a2885dde89103c31
peijyunshih <peijyun.shih@gmail.com>
2016-07-28 14:59:55 +0800

    使用CSS Flexbox實現一个水平布局内嵌套一个垂直布局

--- index.android.js
+++ index.android.js
@@ -18,11 +18,15 @@ class SampleAppMovies extends Component {
  render() {
     var movie = MOCKED_MOVIES_DATA[0];
     return (
-      <View style={styles.container}>
-        <Text>{movie.title}</Text>
-        <Text>{movie.year}</Text>
-        <Image source={{uri: movie.posters.thumbnail}} style={styles.thumbnail}/>
-      </View>
+       <View style={styles.container}>
+          <Image
+            source={{uri: movie.posters.thumbnail}}
+            style={styles.thumbnail}/>
+          <View style={styles.rightContainer}>
+            <Text style={styles.title}>{movie.title}</Text>
+            <Text style={styles.year}>{movie.year}</Text>
+          </View>
+        </View>
     );
   }
 }
@@ -30,9 +34,21 @@ class SampleAppMovies extends Component {
 var styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
+  },
+  rightContainer: {
+    flex: 1,
+  },
+  title: {
+    fontSize: 20,
+    marginBottom: 8,
+    textAlign: 'center',
+  },
+  year: {
+    textAlign: 'center',
   },
   thumbnail: {
     width: 53,
