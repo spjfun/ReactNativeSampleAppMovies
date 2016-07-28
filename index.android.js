dd895e772e81a03da7657b930480e467f538fcdb
peijyunshih <peijyun.shih@gmail.com>
2016-07-28 15:10:51 +0800

    add jsonData

--- index.android.js
+++ index.android.js
@@ -10,9 +10,7 @@ import {
 } from 'react-native';
 
 
-var MOCKED_MOVIES_DATA = [
-  {title: '标题', year: '2015', posters: {thumbnail: 'http://i.imgur.com/UePbdph.jpg'}},
-];
+var REQUEST_URL = 'https://raw.githubusercontent.com/facebook/react-native/master/docs/MoviesExample.json';
 
 class SampleAppMovies extends Component {
  render() {
